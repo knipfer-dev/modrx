65365def96f39a855a9d051f0ce5a79616957ecf
knipferrc <knipferrc@gmail.com>
2020-03-27 21:16:04 -0400

    TextInput component

---
+++ src/components/TextInput/TextInput.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let label = undefined;
+  export let id = undefined;
+  export let value = '';
+  export let name = '';
+  export let isFormField = false;
+  export let fullWidth = false;
+  export let placeholder = '';
+  export let required = false;
+  export let hasError = false;
+  $: classes =
+    'bg-white appearance-none border-2 border-gray-400 rounded w-full px-4 py-3 text-gray-800 leading-tight focus:outline-none focus:border-gray-900 ' +
+    ($$props.class ? $$props.class : '');
+</script>
+
+<div class:w-full={fullWidth} class="flex flex-col">
+  {#if label}
+    <label class="block text-gray-800 font-bold mb-1 uppercase text-sm">
+      {label}
+      {#if required}
+        <span class="text-red-500">*</span>
+      {/if}
+    </label>
+  {/if}
+  <input
+    type="text"
+    {id}
+    {name}
+    {value}
+    {placeholder}
+    {required}
+    on:change
+    on:keyup
+    class:mb-6={isFormField}
+    class:border-red-300={hasError}
+    class={classes} />
+</div>
--- src/pages/Home/Home.svelte
+++ src/pages/Home/Home.svelte
@@ -1,6 +1,10 @@
 <script>
   import Meta from '../../components/Meta/Meta';
+  import TextInput from '../../components/TextInput/TextInput';
 </script>
 
 <Meta title="Home" description="Welcome to ModRX" />
-<h1 class="text-2xl">ModRX</h1>
+
+<div class="p-4">
+  <TextInput placeholder="Search Dealers" />
+</div>
